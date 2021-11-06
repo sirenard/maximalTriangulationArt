7c67c1fa66e75d6e5fff85b1fa60e4a8491500e2
sirenard <renardsimon@skynet.be>
2021-11-06 10:07:05 +0100

    Fix abs function + gitignore

--- js/polygon.js
+++ js/polygon.js
@@ -87,7 +87,8 @@ class Triangle extends Polygon {
     }
 
     area() {
-        let a, b, c = this.points;
-        return 0.5 * abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y))
+        let a, b, c;
+        [a, b, c] = this.points;
+        return 0.5 * Math.abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y))
     }
 }
