f6aaf5b766f6fd1673d82fb64b0e6563594bab80
emmdupui <emma.dupuis@ulb.be>
2021-11-09 15:49:11 +0100

    vertices AC

--- js/minEnclosingTriangle.js
+++ js/minEnclosingTriangle.js
@@ -1,6 +1,7 @@
 class MinEnclosingTriangle {
     constructor(polygon){
         this.polygon = polygon;
+        this.EPSILON = 10**-5;
     }
 
     findMinEnclosingTriangle(){
@@ -9,16 +10,15 @@ class MinEnclosingTriangle {
             for (let j = 0; j < this.polygon[i].length; ++j){
                 if (this.polygon.get(j) !== this.polygon.get(i)){
                     let s2 = [this.polygon.get(j), this.polygon.get(j+1)]; // second side of enclosing triangle
-                    let e1OfTriangle = this.getLineIntersection(s1, s2);
-                    if (e1OfTriangle) { //not parallel lines
+                    let vertexB = this.getLineIntersection(s1, s2);
+                    if (vertexB) { //not parallel lines
                         for (let k = 0; k < this.polygon[i].length; ++k) {
                             if (this.polygon.get(k) !== this.polygon.get(i)
                                 && this.polygon.get(k) !== this.polygon.get(i + 1)
                                 && this.polygon.get(k) !== this.polygon.get(j)
                                 && this.polygon.get(k) !== this.polygon.get(j + 1)) {
-                                //let centroid = this.getTriangleCentroid();
-
-
+                                this.computeVerticesACFromMidpointB(s1,s2,k);
+                                this.computeVerticesACFromFlushB(s1, s2, k);
                             }
                         }
                     }
@@ -26,13 +26,73 @@ class MinEnclosingTriangle {
             }
         }
     }
+    
+    computeVerticesACFromMidpointB(sideC, sideA, k){
+        let sideCParameters = this.getLineEquations(sideC);
+        let a1 = sideCParameters[0];
+        let b1 = sideCParameters[1];
+        let c1 = sideCParameters[2];
+
+        // Side B parameters
+        let sideAParameters = this.getLineEquations(sideA);
+        let a2 = sideAParameters[0];
+        let b2 = sideAParameters[1];
+        let c2 = sideAParameters[2];
+
+        // Polygon point "b" coordinates
+        let m = this.polygon[k].x;
+        let n = this.polygon[k].y;
+
+        // Compute vertices A and C x-coordinates
+        let x2 = ((2 * b1 * b2 * n) + (c1 * b2) + (2 * a1 * b2 * m) + (b1 * c2)) / ((a1 * b2) - (a2 * b1));
+        let x1 = (2 * m) - x2;
+
+        // Compute vertices A and C y-coordinates
+        let y2;
+        let y1 ;
+
+        if (this.tendsTo0(b1)) {          // b1 = 0 and b2 != 0
+            y2 = ((-c2) - (a2 * x2)) / (b2);
+            y1 = (2 * n) - y2;
+        } else if (this.tendsTo0(b2)) {   // b1 != 0 and b2 = 0
+            y1 = ((-c1) - (a1 * x1)) / (b1);
+            y2 = (2 * n) - y1;
+        } else {                                    // b1 != 0 and b2 != 0
+            y1 = ((-c1) - (a1 * x1)) / (b1);
+            y2 = ((-c2) - (a2 * x2)) / (b2);
+        }
+
+        // Update vertices A and C coordinates
+        let vertexA = new Point(x1, y1);
+        let vertexC = new Point(x2, y2);
+
+        return [vertexA, vertexC]
+    }
+
+    getLineEquations(side){
+        /**
+         * From two points defining a line, return parameters a, b, c such that the line equation is ax + by = c
+         * @type {number}
+         */
+        let a = side[1].y - side[0].y;
+        let b = side[0].x - side[1].x;
+        let c = ((-side[0].y) * b) - (side[0].x * a);
+        return [a,b,c]
+    }
+
+    tendsTo0(value){
+        return Math.abs(value) <= this.EPSILON;
+    }
+
+    computeVerticesACFromFlushB(){
+
+    }
 
     getLineIntersection(line1, line2){
         /**
          * finds the intersection of two infinite lines determined by two points.
          * @type {number}
          */
-        // https://www.geeksforgeeks.org/program-for-point-of-intersection-of-two-lines/
         let y1 = line1[1].y - line1[0].y;
         let x1 = line1[0].x - line1[1].x;
         let c1 = y1*line1[0].x + x1*line1[0].y;
@@ -47,17 +107,5 @@ class MinEnclosingTriangle {
             let intersectionY = (y1*c2 - y2*c1)/det;
             return new Point(intersectionX, intersectionY);
         } else return null; //parallel lines
-    }
-
-    getTriangleCentroid(side1, side2, point){
-        /**
-         * get triangle's centroid
-         * side1 and side2 are rays (as we haven't defined the last side of the triangle)
-         * param side1 : first edge of triangle
-         * param side2 : second edge of triangle
-         * param point : center point of the last edge of the triangle
-         */
-
-
     }
 }
