61a28095f61e90833154f97d2e67330239618c63
emmdupui <emma.dupuis@ulb.be>
2021-11-11 13:41:05 +0100

    Changes by simon

--- sketch.js
+++ sketch.js
@@ -41,7 +41,6 @@ function draw() {
             text("Here is your piece of art !", 30, 50);
             points.map(p => p.draw());
             triangles.map(t => {
-                t.fill = color(random(255),random(255),random(255));
                 t.draw();
             });
             break;
@@ -55,6 +54,9 @@ function solve() {
 
     let polygon = new Polygon(points); // thus points = CH(points);
     maximal_triangulation(polygon, triangles);
+    triangles.map(t => t.fill = color(random(255),random(255),random(255)));
+
+    //triangles.push(polygon);
     //compute the smallest enclosing triangle and add it to the triangles list
 
     let enclosingTriangle = new MinEnclosingTriangle(polygon);
